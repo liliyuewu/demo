e0d0e0f08161f94c2ac4dfd068bb667fcf61cbd8
liliyuewu <370883115@qq.com>
2020-09-14 20:42:22 +0800

    修改

--- src/test/java/com/example/demo/DemoApplicationTests.java
+++ src/test/java/com/example/demo/DemoApplicationTests.java
@@ -3,6 +3,7 @@ package com.example.demo;
 import Uitls.CombineUtils;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.web.bind.annotation.ExceptionHandler;
 
 import java.util.*;
 
@@ -10,14 +11,50 @@ import java.util.*;
 class DemoApplicationTests {
 
     /**
-     * 测试
+     * 测试1
      */
     @Test
-    public void test() {
-        Integer[] arrs = {2, 3, 5};
-        //将数字和字符组合转换
-        List<String[]> resultList = CombineUtils.digitsConvertLetters(arrs);
-        //打印输出组合结果
-        CombineUtils.printResult(resultList);
+    public void testOne() {
+        try{
+            Integer[] arrs = {2, 3, 5};
+            //将数字和字符组合转换
+            List<String[]> resultList = CombineUtils.digitsConvertLetters(arrs);
+            //打印输出组合结果
+            CombineUtils.printResult(resultList);
+        }catch (Exception e){
+            throw new RuntimeException(e);
+        }
+    }
+
+    /**
+     *测试2
+     */
+    @Test
+    public void testTwo() {
+        try{
+            Integer[] arrs = {0, 9};
+            //将数字和字符组合转换
+            List<String[]> resultList = CombineUtils.digitsConvertLetters(arrs);
+            //打印输出组合结果
+            CombineUtils.printResult(resultList);
+        }catch (Exception e){
+            throw new RuntimeException(e);
+        }
+    }
+
+    /**
+     *测试3
+     */
+    @Test
+    public void testThree() {
+        try{
+            Integer[] arrs = {0, 9, 2, 4};
+            //将数字和字符组合转换
+            List<String[]> resultList = CombineUtils.digitsConvertLetters(arrs);
+            //打印输出组合结果
+            CombineUtils.printResult(resultList);
+        }catch (Exception e){
+            throw new RuntimeException(e);
+        }
     }
 }
